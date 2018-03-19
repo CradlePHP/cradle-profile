2a646e9a77c3a3bdc3fd4404fae28594531fc7db
Christian Blanquera <cblanquera@openovate.com>
2018-03-19 12:47:10 +0800

    de-level events

--- src/events.php
+++ src/events.php
@@ -3,52 +3,50 @@
  * This file is part of a Custom Package.
  */
 
-return function ($request, $response) {
-    /**
-     * Creates a profile
-     *
-     * @param Request $request
-     * @param Response $response
-     */
-    $this->on('profile-create', function ($request, $response) {});
+/**
+ * Creates a profile
+ *
+ * @param Request $request
+ * @param Response $response
+ */
+$this->on('profile-create', function ($request, $response) {});
 
-    /**
-     * Creates a profile
-     *
-     * @param Request $request
-     * @param Response $response
-     */
-    $this->on('profile-detail', function ($request, $response) {});
+/**
+ * Creates a profile
+ *
+ * @param Request $request
+ * @param Response $response
+ */
+$this->on('profile-detail', function ($request, $response) {});
 
-    /**
-     * Removes a profile
-     *
-     * @param Request $request
-     * @param Response $response
-     */
-    $this->on('profile-remove', function ($request, $response) {});
+/**
+ * Removes a profile
+ *
+ * @param Request $request
+ * @param Response $response
+ */
+$this->on('profile-remove', function ($request, $response) {});
 
-    /**
-     * Restores a profile
-     *
-     * @param Request $request
-     * @param Response $response
-     */
-    $this->on('profile-restore', function ($request, $response) {});
+/**
+ * Restores a profile
+ *
+ * @param Request $request
+ * @param Response $response
+ */
+$this->on('profile-restore', function ($request, $response) {});
 
-    /**
-     * Searches profile
-     *
-     * @param Request $request
-     * @param Response $response
-     */
-    $this->on('profile-search', function ($request, $response) {});
+/**
+ * Searches profile
+ *
+ * @param Request $request
+ * @param Response $response
+ */
+$this->on('profile-search', function ($request, $response) {});
 
-    /**
-     * Updates a profile
-     *
-     * @param Request $request
-     * @param Response $response
-     */
-    $this->on('profile-update', function ($request, $response) {});
-};
+/**
+ * Updates a profile
+ *
+ * @param Request $request
+ * @param Response $response
+ */
+$this->on('profile-update', function ($request, $response) {});
