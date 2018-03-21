73539755ca2583fcfd631ecfb1dfda982383a9b8
Charles Zamora <charles.andacc@gmail.com>
2018-03-21 18:52:41 +0800

    remove install helpers from preprocessor

--- .cradle.php
+++ .cradle.php
@@ -5,6 +5,4 @@
 require_once __DIR__ . '/package/events.php';
 require_once __DIR__ . '/src/events.php';
 require_once __DIR__ . '/src/controller.php';
-
-//bootstrap
-$this->preprocess(include __DIR__ . '/package/helpers.php');
+require_once __DIR__ . '/package/helpers.php';
